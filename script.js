767e778f38b5d09963fa1620bd45a217814168dd
Haikalbotz <97148397+Haikalbotz@users.noreply.github.com>
2025-02-06 13:01:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('nav ul li a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href');
+            const targetSection = document.querySelector(targetId);
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+
+            // Animasi modern untuk navbar
+            links.forEach(link => link.classList.remove('active'));
+            this.classList.add('active');
+        });
+    });
+});
